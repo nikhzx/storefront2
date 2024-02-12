845989dc1ae8725ddbbc13b765bb08ffd358b92c
Nikhil Jaiswal <nikhzx@gmail.com>
2024-02-12 23:15:52 +0530

    Changes for Serializer

--- store/views.py
+++ store/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Product
+from.serializers import ProductSerializer
 
 # Create your views here.
+#func based views
+@api_view()
+def product_list(request):
+    return Response("OK")
+
+@api_view()
+def product_detail(request, id):
+    product = Product.objects.get(pk=id)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
